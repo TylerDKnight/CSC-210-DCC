217efab3dfc9f696f6954c121463fadcd8678c50
TylerDKNight <TylerDKnight@github.com>
2016-10-14 05:49:27 -0400

    wanks

--- www/cgi-bin/login.py
+++ www/cgi-bin/login.py
@@ -3,10 +3,12 @@
 import os, cgi, cgitb; cgitb.enable()
 
 fieldData = cgi.FieldStorage()
-#loginInfo = fieldData['username'].value
-#passInfo = fieldData.value('password')
+loginInfo = fieldData['username'].value
+passInfo = fieldData['password'].value
 returnHTML = "Error: return HTML not assigned"
+##success html TBD
 successfulHTML = "YES"
+##Same as Login page HTML but with failed message
 failedHTML = "<html><head><title>login here</title></head><body><h1 style=\"color:red;\">Login failed!</h1><br/><form action=\"/cgi-bin/login.py\" method=\"POST\"><h1>Username:</h1><br/><input type=\"text\" name=\"userfield\"/><h1>Password:</h1><br/><input type=\"password\" name=\"passwordfield\"/><br/><br/><input type=submit value=\"login\"/></form></body></html>"
 
 def auth(login, password):
